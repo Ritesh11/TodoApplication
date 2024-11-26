6126dacfac01821d2ffde183ec93b64c38501d82
RITESH <riteshksingh76@gmail.com>
2024-11-26 22:38:52 +0530

    First App

--- app/src/main/java/com/rks/todoapplication/MainActivity.kt
+++ app/src/main/java/com/rks/todoapplication/MainActivity.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Divider
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
