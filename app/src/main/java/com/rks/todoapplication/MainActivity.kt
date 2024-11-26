8736b421b503f8cbf7a21b2ca92d4203949c3a93
RITESH <riteshksingh76@gmail.com>
2024-11-26 22:38:31 +0530

    First App

--- app/src/main/java/com/rks/todoapplication/MainActivity.kt
+++ app/src/main/java/com/rks/todoapplication/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -134,7 +135,7 @@ fun ItemList(
             .fillMaxWidth()
             .padding(5.dp)
     )
-    Divider()
+    HorizontalDivider()
 
 }
 
